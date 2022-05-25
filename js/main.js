7c3978bca968616c90367ccf1089950bf8d67789
nhan <phamduynhan16121995@gmail.com>
2022-05-25 14:57:21 +0700

    finish-update-menu-about-mobile

--- js/main.js
+++ js/main.js
@@ -18,3 +18,22 @@ var carouselIndex = 0;
       };
       setInterval(autoChangeImg, 4000);
 
+/*==================== ACCORDION ABOUT ====================*/
+const skillsContent = document.getElementsByClassName('about__content__menu'),
+      skillsHeader = document.querySelectorAll('.about__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i= 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'about__content__menu about__close'
+    }
+
+    if(itemClass === 'about__content__menu about__close'){
+        this.parentNode.className = 'about__content__menu about__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
